2c338d0f368c317d6537b7179ca6453fdb95a5c1
Sam <sam.yang01@gmail.com>
2018-02-01 19:20:17 -0600

    update

--- common_students.py
+++ common_students.py
@@ -24,12 +24,18 @@ while True:
         break
 
 
-#check to see if students are in both python and web applications class
+'''check to see if students are in both python and web applications class
+use a for loop to check to see if each student in python is also in web applications.
+If students are in both classes then append to 'common students' list.  If students
+are not in boh classes then append to 'disjoint students' list'''
 for student in python_students:
     if student in web_students:
         common_students.append(student)
     else:
         disjoint_students.append(student)
+'''since we already checked the python student list for students taking both classes, we
+only need to check the web application student list for students not taking both classes and 
+append to 'disjoint student list'''
 for student in web_students:
     if student not in python_students:
         disjoint_students.append(student)
